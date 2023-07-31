41fd41bef665ef42be2dfdd521c6c17b56c86b1a
Guz013 <43732358+Guz013@users.noreply.github.com>
2023-07-31 19:01:59 -0300

    feat(cli): ✨ project structure detection

---
+++ packages/cli/src/types.d.ts
@@ -0,0 +1,17 @@
+
+interface Options {
+	environment: {
+		node: boolean
+		deno: boolean
+		browser: boolean
+	}
+}
+
+export interface Workspace {
+	packages: Package[]
+}
+
+export interface Package {
+	name: string
+	files: string[]
+}
